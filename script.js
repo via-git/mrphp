f584038fb267b84c119d3997efefb8c665e4013f
via-git <126256445+via-git@users.noreply.github.com>
2023-06-01 21:35:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
- document.getElementById('upload-form').addEventListener('submit', function(e) {
-  e.preventDefault();
+$(document).ready(function() {
+  $('#upload-form').on('submit', function(e) {
+    e.preventDefault();
 
-  var fileInput = document.getElementById('file-input');
-  var file = fileInput.files[0];
-  var chatId = '1184220694'; // Ganti dengan chat ID tujuan
+    var fileInput = $('#file-input')[0];
+    var file = fileInput.files[0];
+    var chatId = '1184220694'; // Ganti dengan chat ID tujuan
 
-  var formData = new FormData();
-  formData.append('file', file);
-  formData.append('chat_id', chatId);
+    var formData = new FormData();
+    formData.append('file', file);
+    formData.append('chat_id', chatId);
 
-  // Kirim file menggunakan AJAX
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://api.telegram.org/bot6072635225:AAHV_Sej8JmCptHtWp3JElwGmAm6RHTpIHA/sendDocument', true);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      alert('File berhasil dikirim!');
-    } else {
-      alert('Terjadi kesalahan saat mengirim file.');
-    }
-  };
-  xhr.send(formData);
+    // Kirim file menggunakan AJAX
+    $.ajax({
+      url: 'https://api.telegram.org/bot6072635225:AAHV_Sej8JmCptHtWp3JElwGmAm6RHTpIHA/sendDocument',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(response) {
+        alert('File berhasil dikirim!');
+      },
+      error: function(xhr, status, error) {
+        alert('Terjadi kesalahan saat mengirim file.');
+      }
+    });
+  });
 });
