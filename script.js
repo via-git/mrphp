9ffd6ba5aa725ebe8372047c3587a5502eda5103
via-git <126256445+via-git@users.noreply.github.com>
2023-06-01 21:11:46 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+ document.getElementById('upload-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  var fileInput = document.getElementById('file-input');
+  var file = fileInput.files[0];
+  var chatId = '1184220694'; // Ganti dengan chat ID tujuan
+
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('chat_id', chatId);
+
+  // Kirim file menggunakan AJAX
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://api.telegram.org/bot<YOUR_BOT_TOKEN>/sendDocument', true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      alert('File berhasil dikirim!');
+    } else {
+      alert('Terjadi kesalahan saat mengirim file.');
+    }
+  };
+  xhr.send(formData);
+});
